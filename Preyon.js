e047ca3f941da9476ceecc1a470f23944b9e8cd6
Jongmin Kim <cmiscm@gmail.com>
2016-10-20 20:08:57 -0700

    Add files via upload

--- Preyon.js
+++ Preyon.js
@@ -176,7 +176,7 @@ var Preyon = Preyon || (function () {
             var labelCon = new CMControl.Panel();
             container.add(labelCon);
             item = new CMControl.Panel();
-            item.setHTMLContent('<a href="http://blog.cmiscm.com/?p=5325">BLOG.CMISCM.COM</a>');
+            item.setHTMLContent('<a href="http://blog.cmiscm.com/?p=5325" target="_blank">BLOG.CMISCM.COM</a>');
             item.setClass('link');
             labelCon.add(item);
 
